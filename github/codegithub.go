b3a58b23ee02d0380b0c21331063ae0c966e6ca5
Aerith Gainsborough <aerith86@proton.me>
2023-04-25 10:52:18 -0600

    Adding GitHub package to trevmod

---
+++ github/codegithub.go
@@ -0,0 +1,11 @@
+package github
+
+import "fmt"
+
+func DoGitHubStuff() {
+	fmt.Println("Doing work against GitHub REST API")
+}
+
+func doPrivateGitHubStuff() {
+	fmt.Println("Doing private work against GitHub REST API")
+}
